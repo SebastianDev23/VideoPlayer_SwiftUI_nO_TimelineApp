b83111c64e3f64471323d9ada739de23590835f4
Sebastian <s.kloesel@gmx.de>
2021-12-29 13:42:42 +0100

    Initial Commit

---
+++ VideoPlayer_SwiftUI_nO_Timeline/VideoPlayer_SwiftUI_nO_TimelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayer_SwiftUI_nO_TimelineApp.swift
+//  VideoPlayer_SwiftUI_nO_Timeline
+//
+//  Created by Sebastian Klösel on 29.12.21.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayer_SwiftUI_nO_TimelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
